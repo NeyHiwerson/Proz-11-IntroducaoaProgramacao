0d9eeaaba68d518afe7ed75b41439839052867b6
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2023-07-04 19:10:28 -0300

    primeiro commit

---
+++ Modulo01/07 - aluno aprovado/arquivo.py
@@ -0,0 +1,6 @@
+#Problema: Crie um programa que calcule o status
+# de aprovação do aluno a partir da nota dele.
+# O código deve seguir as instruções:
+#nota menor que 4: ele está reprovado;
+#nota menor que 7: ele está em recuperação;
+#nota maior que 7: ele está aprovado.
