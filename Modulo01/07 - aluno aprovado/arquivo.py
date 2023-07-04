b63a15b28d33b575b44ff8d623936539b3a2317a
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2023-07-04 19:31:48 -0300

    resolucao exe aluno aprovado

--- Modulo01/07 - aluno aprovado/arquivo.py
+++ Modulo01/07 - aluno aprovado/arquivo.py
@@ -4,3 +4,17 @@
 #nota menor que 4: ele está reprovado;
 #nota menor que 7: ele está em recuperação;
 #nota maior que 7: ele está aprovado.
+
+nota = 7
+
+if (nota < 4):
+
+   print("Reprovado")
+
+elif (nota < 7):
+
+   print("Em recuperação")
+
+elif(nota >=7):
+
+    print("Aprovado")
