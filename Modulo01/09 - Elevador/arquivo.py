6aa8fe09bd21524dfcf79a828ede08d94189aeb0
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2023-07-07 19:19:18 -0300

    resolucao final exe Elevador

--- Modulo01/09 - Elevador/arquivo.py
+++ Modulo01/09 - Elevador/arquivo.py
@@ -1,3 +1,9 @@
+for numero in range(20, -1, -1):
+  if (numero !=13 and numero != 0):
+    print(numero)
+  elif numero == 0:
+    print("T")
+
 andar = 20
 while (andar >= 0):
   if andar == 13:
@@ -8,3 +14,19 @@ while (andar >= 0):
   else:
     print(andar)
     andar = andar - 1
+
+
+andar = 20
+
+while True:
+  if andar == 13:
+    andar = andar - 1
+  elif andar == 0:
+    print("T")
+    andar = andar - 1
+  else:
+    print(andar)
+    andar = andar - 1
+
+  if andar < 0:
+    break
