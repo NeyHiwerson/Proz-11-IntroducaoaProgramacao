6155a7b50bb0e490b42ca13bba6ecebf7ae74934
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2023-07-07 19:00:36 -0300

    resolucao exe Elevador

---
+++ Modulo01/09 - Elevador/arquivo.py
@@ -0,0 +1,10 @@
+andar = 20
+while (andar >= 0):
+  if andar == 13:
+    andar = andar - 1
+  elif andar == 0:
+    print("T")
+    andar = andar - 1
+  else:
+    print(andar)
+    andar = andar - 1
