13abedb5e34749dd1318c7d739137043fea6a53d
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2023-08-20 16:19:50 -0300

    adicionado ano atual ao footer via javaScript

--- Modulo03 - Javascript/03-aulaIntroDOM/scripts/index.js
+++ Modulo03 - Javascript/03-aulaIntroDOM/scripts/index.js
@@ -13,3 +13,14 @@ const textosPorClasse = document.querySelectorAll(".texto-simples");
 console.log(textosPorClasse);
 console.log(textosPorClasse[0]);
 console.log(textosPorClasse[1]);
+
+const myElement = document.getElementById("exemplo");
+myElement.style.color = "red";
+document.getElementById("exemplo").style.color = "green";
+
+document.getElementsByClassName("texto-simples")[0].style.color = "yellow";
+document.getElementsByName("divSegTitulo")[0].style.color = "blue";
+document.getElementsByTagName("p")[0].style.color = "lightgreen";
+let copy = document.getElementsByTagName("span");
+copy[0].innerText = `Copiright ${new Date().getFullYear()}`;
+console.log(copy[0]);
