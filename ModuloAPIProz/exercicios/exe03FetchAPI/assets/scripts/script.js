72d218d5d590a3502959d5a567aef56f16ee7648
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2023-12-06 17:22:48 -0300

    finalizado blog de anuncio de eletronicos

--- ModuloAPIProz/exercicios/exe03FetchAPI/assets/scripts/script.js
+++ ModuloAPIProz/exercicios/exe03FetchAPI/assets/scripts/script.js
@@ -1,32 +1,11 @@
-const nomeProduto = document.getElementById('nome-produto');
-const valorProduto = document.getElementById('valor-produto');
-const descricaoProduto = document.getElementById('descricao-produto');
-const btnEnviar = document.getElementById('btn-anunciar');
-const feedbackUsuario = document.getElementById('feedback-usuario');
-const produtosCadastrados = document.getElementById('produtos-cadastrados');
-const baseURL = 'https://httpbin.org';
-const produtos = [
-  {
-    produto: 'sabonete',
-    valor: '200',
-    descricao: 'faz espuma'
-  },
-  {
-    produto: 'sabonete',
-    valor: '200',
-    descricao: 'faz espuma'
-  },
-  {
-    produto: 'sabonete',
-    valor: '200',
-    descricao: 'faz espuma'
-  },
-  {
-    produto: 'sabonete',
-    valor: '200',
-    descricao: 'faz espuma'
-  },
-];
+const nomeProduto = document.getElementById("nome-produto");
+const valorProduto = document.getElementById("valor-produto");
+const descricaoProduto = document.getElementById("descricao-produto");
+const btnEnviar = document.getElementById("btn-anunciar");
+const feedbackUsuario = document.getElementById("feedback-usuario");
+const produtosCadastrados = document.getElementById("produtos-cadastrados");
+const baseURL = "https://httpbin.org";
+const produtos = [];
 
 /* Modelo de produto
   const produto = {
@@ -35,34 +14,93 @@ const produtos = [
   descricao: ''
 }; */
 
+function formatarValorEmReais(valor) {
+  const valorFormatado = valor.toFixed(2).replace(".", ",");
+  return valorFormatado;
+}
+
 /* Função para exibir os produtos na section produto-cadastrado */
-function exibeProdutos(){
-  produtosCadastrados.innerHTML = '';
-  produtos.forEach(produto => {
-    const produtoCadastrado = document.createElement('div');
-    produtoCadastrado.classList.add('produto-cadastrado');
+function exibeProdutos() {
+  produtosCadastrados.innerHTML = "";
+  produtos.forEach((produto) => {
+    console.log(typeof produto.valor);
+    const valorFormatado = formatarValorEmReais(Number(produto.valor));
+    const produtoCadastrado = document.createElement("div");
+    produtoCadastrado.classList.add("produto-cadastrado");
     produtoCadastrado.innerHTML = `
     <h2>${produto.produto}</h2>
-    <p>R$ ${produto.valor} reais.</p>
+    <p>R$ ${valorFormatado} reais.</p>
     <p>${produto.descricao}</p>
-    </div>`
+    </div>`;
     produtosCadastrados.prepend(produtoCadastrado);
   });
-};
+}
 /* Função para atualizar a lista de produtos cadastrados no array produtos */
-function atualizaProdutos(){
-
-};
+function atualizaProdutos() {}
 
 /* Função para cadastrar um novo produto na API backEnd */
-function cadastrarProduto(){
-
-};
+function cadastrarProduto(produto) {
+  const prodCadastrado = fetch(`${baseURL}/post`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(produto), //aqui transforma javascript em stringJson
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      produtos.push(JSON.parse(data.data)); //aqui transforma stringJson em javascript
+      exibeProdutos();
+      nomeProduto.value = "";
+      valorProduto.value = "";
+      descricaoProduto.value = "";
+      feedbackUsuario.innerText = "Produto cadastrado com sucesso!";
+      setTimeout(() => {
+        feedbackUsuario.innerText = "";
+      }, 3000);
+    })
+    .catch((error) => {
+      console.log(error);
+      feedbackUsuario.innerText = "Erro ao cadastrar produto";
+      setTimeout(() => {
+        feedbackUsuario.innerText = "";
+      }, 3000);
+    });
+}
 
 /* evento para cadastrar um novo produto  */
-btnEnviar.addEventListener('click', () => {
-  cadastrarProduto();
-  atualizaProdutos();
-  exibeProdutos();
+btnEnviar.addEventListener("click", async function (evento) {
+  evento.preventDefault();
+  const regex = /^\d+(\.\d+)?$/;
+  if (!regex.test(valorProduto.value)) {
+    feedbackUsuario.innerText = "Valor inválido!";
+    setTimeout(() => {
+      feedbackUsuario.innerText = "";
+    }, 3000);
+    return;
+  }
+  if (
+    nomeProduto.value == "" ||
+    nomeProduto.value == undefined ||
+    valorProduto.value == "" ||
+    valorProduto.value == undefined ||
+    descricaoProduto.value == "" ||
+    descricaoProduto.value == undefined
+  ) {
+    feedbackUsuario.innerText = "Preencha todos os campos!";
+    setTimeout(() => {
+      feedbackUsuario.innerText = "";
+    }, 3000);
+  } else {
+    const produto = {
+      produto: nomeProduto.value,
+      valor: valorProduto.value,
+      descricao: descricaoProduto.value,
+    };
+    try {
+      cadastrarProduto(produto);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 });
-exibeProdutos();
