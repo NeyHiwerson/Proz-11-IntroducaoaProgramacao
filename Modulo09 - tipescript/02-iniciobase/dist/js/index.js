19cde71fa09b58b9fb922c74c57d07f15bc3a8e0
Ney Hiwerson Missias Ribeiro <hiwerson@gmail.com>
2024-03-18 22:57:25 -0300

    testes de tipagem simples, tipagem de dom e a tipagem any

--- Modulo09 - tipescript/02-iniciobase/dist/js/index.js
+++ Modulo09 - tipescript/02-iniciobase/dist/js/index.js
@@ -1,8 +1,47 @@
 "use strict";
-const nome = "João Paulo";
+const nome = "João Paulo Bernadino";
 const idade = 30;
-const ativo = false;
+let ativo = true;
 const data = new Date();
-const matriculado = ativo ? "está matriculado" : "não está matriculado";
-const mensagem = `O aluno ${nome}, idade ${idade} anos, ${matriculado} nesta academia.`;
+function obterStatusMatricula(ativo) {
+    return ativo ? "está matriculado" : "não está matriculado";
+}
+const mensagem = `O aluno ${nome}, idade ${idade} anos,
+na data ${data.toLocaleDateString("pt-BR")} ${obterStatusMatricula(ativo)} nesta academia.`;
 console.log(mensagem);
+const btnVerificaAluno = document.querySelector("#btnVerificaAluno");
+btnVerificaAluno === null || btnVerificaAluno === void 0 ? void 0 : btnVerificaAluno.addEventListener("click", () => {
+    const inputNomeCompleto = document.querySelector("#inputNomeCompleto");
+    verificaAluno((inputNomeCompleto === null || inputNomeCompleto === void 0 ? void 0 : inputNomeCompleto.value) || null);
+});
+function verificaAluno(inputNome) {
+    if (inputNome === nome) {
+        ativo = true;
+        alert(`O aluno ${inputNome}, na data ${data.toLocaleDateString("pt-BR")}
+         ${obterStatusMatricula(ativo)} nesta academia.`);
+    }
+    else {
+        ativo = false;
+        alert(`O aluno ${inputNome}, na data ${data.toLocaleDateString("pt-BR")}
+      ${obterStatusMatricula(ativo)} nesta academia.`);
+    }
+}
+//***********************
+const titulo = document.querySelector('h1');
+// na expressão abaixo bem genérica esse é o retorno: Element | null
+const link = document.querySelector('#linkId');
+//porem se vc for usar algum metodo do elemento ele vai acusar erro
+//por isso usamos o cast: (link as HTMLAnchorElement).href;
+//podemos usar o typeof para verificar o tipo de dado
+//console.log(typeof link);
+//podemos usar o any para receber qualquer tipo de dado
+//o any faz a variavel se comportar como javascript
+let tudo = "teste";
+console.log(typeof tudo);
+console.log(tudo);
+tudo = 10;
+console.log(typeof tudo);
+console.log(tudo);
+tudo = true;
+console.log(typeof tudo);
+console.log(tudo);
--- Modulo09 - tipescript/02-iniciobase/src/ts/index.ts
+++ Modulo09 - tipescript/02-iniciobase/src/ts/index.ts
@@ -1,7 +1,55 @@
-const nome: string = "João Paulo";
+const nome: string = "João Paulo Bernadino";
 const idade: number = 30;
-const ativo: boolean = false;
+let ativo: boolean = true;
 const data: Date = new Date();
-const matriculado: string = ativo? "está matriculado": "não está matriculado";
-const mensagem: string = `O aluno ${nome}, idade ${idade} anos, ${matriculado} nesta academia.`;
- console.log(mensagem);
+
+function obterStatusMatricula(ativo: boolean): string {
+  return ativo ? "está matriculado" : "não está matriculado";
+}
+
+const mensagem: string = `O aluno ${nome}, idade ${idade} anos,
+na data ${data.toLocaleDateString("pt-BR")} ${obterStatusMatricula(ativo)} nesta academia.`;
+
+console.log(mensagem);
+
+const btnVerificaAluno: HTMLButtonElement | null = document.querySelector("#btnVerificaAluno");
+btnVerificaAluno?.addEventListener("click", () => {
+    const inputNomeCompleto: HTMLInputElement | null = document.querySelector("#inputNomeCompleto");
+    verificaAluno(inputNomeCompleto?.value || null);
+});
+
+function verificaAluno(inputNome: string | null) {
+    if (inputNome === nome) {
+        ativo = true;
+        alert(`O aluno ${inputNome}, na data ${data.toLocaleDateString("pt-BR")}
+         ${obterStatusMatricula(ativo)} nesta academia.`);
+    } else {
+        ativo = false;
+        alert(`O aluno ${inputNome}, na data ${data.toLocaleDateString("pt-BR")}
+      ${obterStatusMatricula(ativo)} nesta academia.`);
+    }
+}
+
+//***********************
+const titulo: HTMLHeadingElement | null = document.querySelector('h1');
+
+// na expressão abaixo bem genérica esse é o retorno: Element | null
+const link = document.querySelector('#linkId');
+//porem se vc for usar algum metodo do elemento ele vai acusar erro
+//por isso usamos o cast: (link as HTMLAnchorElement).href;
+
+//podemos usar o typeof para verificar o tipo de dado
+//console.log(typeof link);
+
+//podemos usar o any para receber qualquer tipo de dado
+//o any faz a variavel se comportar como javascript
+
+let tudo: any = "teste";
+console.log(typeof tudo);
+console.log(tudo);
+tudo = 10;
+console.log(typeof tudo);
+console.log(tudo);
+tudo = true;
+console.log(typeof tudo);
+console.log(tudo);
